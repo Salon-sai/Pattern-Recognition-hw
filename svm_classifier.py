5813d37526e2cb8de1a376f44268f563121faa8a
Salon.sai <coke1231078@gmail.com>
2017-04-27 09:32:49 +0800

    fix the classifier

--- svm_classifier.py
+++ svm_classifier.py
@@ -1,6 +1,6 @@
 import data_process
 from sklearn import svm
-from sklearn import cross_validation
+from sklearn import model_selection
 from sklearn.naive_bayes import GaussianNB
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
@@ -19,11 +19,16 @@ esimators['svm_linear'] = svm.LinearSVC()
 
 for k in esimators.keys():
     start_time = datetime.datetime.now()
-
-    print()
-
+    print('-----%s------', k)
+    esimators[k] = esimators[k].fit(data_process.all_train_data, data_process.all_train_label)
+    predict = esimators[k].predict(data_process.all_test_data)
+    print('%s Score: %0.4f' % (k, esimators[k].score(data_process.all_test_data, data_process.all_test_label)))
+    print("%s test accuracy %0.4f" % (k, accuracy_score(data_process.all_test_label, predict)))
+    # scores = model_selection.cross_val_score(esimators[k], data_process.all_test_data, data_process.all_test_label, cv=5)
+    # print("%s Cross Avg. Score: %0.4f (+/- %0.4f)" %(k, scores.mean(), scores.std() * 2))
     end_time = datetime.datetime.now()
     spend = end_time - start_time
+    print("%s Time: %0.2f" % (k, spend.total_seconds()))
 
 # clf = svm.SVC(C=0.5)
 # clf.fit(data_process.all_train_data, data_process.all_train_label)
